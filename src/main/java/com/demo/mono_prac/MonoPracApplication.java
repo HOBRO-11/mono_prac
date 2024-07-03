b354ff9c5bc65582da228698320f8d7a1503720b
Hobro <hobro3710@naver.com>
2024-07-03 20:41:34 +0900

    Divide Application.yml for DB

--- src/main/java/com/demo/mono_prac/MonoPracApplication.java
+++ src/main/java/com/demo/mono_prac/MonoPracApplication.java
@@ -2,8 +2,10 @@ package com.demo.mono_prac;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class MonoPracApplication {
 
 	public static void main(String[] args) {
